82d1d5e7ac533c7bac7bc90696a69313102030c9
Fernando <100nandoo@gmail.com>
2022-04-16 11:12:57 +0800

    [TEMP] change app id for now

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
 const val PUBLISHING_GROUP = "org.redaksi.pillar"
 
 object AppCoordinates {
-    const val APP_ID = "org.redaksi.pillar"
+    const val APP_ID = "org.redaksi.pillar.new"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
