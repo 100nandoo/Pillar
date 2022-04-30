a0f728f1dfa2755d1dfdaf039edd03bbf49074dd
Fernando <100nandoo@gmail.com>
2022-04-30 11:09:50 +0800

    New Release! 🎊🎉

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,10 +1,10 @@
 const val PUBLISHING_GROUP = "org.redaksi.pillar"
 
 object AppCoordinates {
-    const val APP_ID = "org.redaksi.pillar.new"
+    const val APP_ID = "org.redaksi.pillar"
 
-    const val APP_VERSION_NAME = "1.0.0"
-    const val APP_VERSION_CODE = 1
+    const val APP_VERSION_NAME = "2.0.0"
+    const val APP_VERSION_CODE = 17
 
     const val minSDK = 21
     const val targetSDK = 31
