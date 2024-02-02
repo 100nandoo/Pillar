275bd456ecb2c865f849b2b508b49c3a75b5397a
Fernando <100nandoo@gmail.com>
2024-02-02 17:38:18 +0800

    Update to Kotlin 1.9.22

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("android")
     kotlin("kapt")
-    kotlin("plugin.serialization") version "1.9.20"
+    kotlin("plugin.serialization") version libs.versions.kotlin.get()
 
     id("com.android.library")
     id("dagger.hilt.android.plugin")
