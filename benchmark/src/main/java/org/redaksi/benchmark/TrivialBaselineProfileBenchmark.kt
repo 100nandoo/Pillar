c2d8aad87c44a0ce4d0c3ce5658ec6f868cc7ddc
Fernando <100nandoo@gmail.com>
2024-06-20 12:46:27 +0800

    disable benchmark

--- benchmark/src/main/java/org/redaksi/benchmark/TrivialBaselineProfileBenchmark.kt
+++ benchmark/src/main/java/org/redaksi/benchmark/TrivialBaselineProfileBenchmark.kt
@@ -1,20 +1,20 @@
 package org.redaksi.benchmark
-
-import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
-import androidx.benchmark.macro.junit4.BaselineProfileRule
-import org.junit.Rule
-import org.junit.Test
-
-@OptIn(ExperimentalBaselineProfilesApi::class)
-class TrivialBaselineProfileBenchmark {
-    @get:Rule
-    val baselineProfileRule = BaselineProfileRule()
-
-    @Test
-    fun startup() = baselineProfileRule.collectBaselineProfile(
-        packageName = "org.redaksi.pillar",
-        profileBlock = {
-            startActivityAndWait()
-        }
-    )
-}
+//
+// import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
+// import androidx.benchmark.macro.junit4.BaselineProfileRule
+// import org.junit.Rule
+// import org.junit.Test
+//
+// @OptIn(ExperimentalBaselineProfilesApi::class)
+// class TrivialBaselineProfileBenchmark {
+//     @get:Rule
+//     val baselineProfileRule = BaselineProfileRule()
+//
+//     @Test
+//     fun startup() = baselineProfileRule.collectBaselineProfile(
+//         packageName = "org.redaksi.pillar",
+//         profileBlock = {
+//             startActivityAndWait()
+//         }
+//     )
+// }
